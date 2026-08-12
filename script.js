47ff8c197bf101b669a6faedee080fc23a0005af
2357769 <111243785+2357769@users.noreply.github.com>
2026-08-12 15:38:24 -0500

    create tomodachi life page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function loadFooter(){
     console.log("adding footer");
     $("#content").append("<footer></footer>");
-    $("footer").load("footer.txt");
+    $("footer").load("/footer.txt");
 }
 
 $(function(){
